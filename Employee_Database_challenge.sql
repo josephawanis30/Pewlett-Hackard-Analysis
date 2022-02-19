de51b39a892eed6ffddec3dbace6e02653cab331
josephawanis30 <94503395+josephawanis30@users.noreply.github.com>
2022-02-18 18:18:57 -0700

    Add files via upload

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,54 @@
+-- Deliverable 1.
+SELECT e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   t.title,
+	   t.from_date,
+	   t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Distinct
+SELECT DISTINCT ON (emp_no) emp_no,
+	   first_name,
+	   last_name,
+	   title
+INTO unique_titles
+FROM retirement_titles as r
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC; 
+
+SELECT * FROM retiring_titles;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   t.title,
+	   de.to_date
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM mentorship_eligibility;
+
